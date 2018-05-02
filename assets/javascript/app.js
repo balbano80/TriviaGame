526d73c14c768d83d78daf26504fd698636879f0
balbano80 <balbano80@gmail.com>
2018-05-01 18:00:23 -0700

    added some basic setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,61 @@
+correct = 0;
+wrong = 0;
+unanswered = 0;
+
+var questionArr = [
+    questionOne = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionTwo = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionThree = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionFour = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionFive = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionSix = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionSeven= {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionEight = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionEight = {
+        question: "",
+        possibles: [],
+        correct: ""
+    },
+    questionEight = {
+        question: "",
+        possibles: [],
+        correct: ""
+    }
+];
+$(document).ready(function() {
+
+
+
+});
